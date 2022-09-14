9316a3cd9ce240db728f02000635de19d7c0e490
rangaomkaram <omkaram.ranga@gmail.com>
2022-09-14 22:52:20 +0530

    update 1

--- MySQL/ConnectMySQL.py
+++ MySQL/ConnectMySQL.py
@@ -10,3 +10,23 @@
 GET CONNECTED WITH MYSQL
 """
 ###################################################################
+
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="Ranga",
+  password="Ranga@1993!",
+  database = "ROYAL_BANK_AUS"
+)
+
+mycursor = mydb.cursor()
+"""mycursor.execute('SELECT * FROM ACCT_BASE LIMIT 7')
+
+
+"""
+mycursor.execute('SHOW COLUMNS FROM ACCT_BASE')
+
+print(mydb)
+for table in mycursor:
+    print(table)
