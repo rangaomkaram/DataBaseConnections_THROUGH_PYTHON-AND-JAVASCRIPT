b1aacc934cf83825cc1e0b5ff0b50bb7e3092423
rangaomkaram <omkaram.ranga@gmail.com>
2022-09-14 10:45:52 +0530

    MySQL connection

---
+++ MySQL/ConnectMySQL.py
@@ -0,0 +1,12 @@
+"""
+1. Connect the MySql using Python
+2. Retrive the Data from Database from MySQL  
+3. Do Some Operation and Manpulations on Data.
+
+"""
+
+####################################################################
+"""
+GET CONNECTED WITH MYSQL
+"""
+###################################################################
